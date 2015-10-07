0e0a498ac554da326f1991202f02e3b4cc287434
Alex de la Rosa <alexdelarosacortes@gmail.com>
2015-10-07 14:43:45 -0500

    intercambio de renglon y cambio de la matriz a double

--- main.cpp
+++ main.cpp
@@ -4,41 +4,66 @@
 
 using namespace std;
 
+void intercambioRenglon(vector<vector<double>> &dMatriz, int iRen1, int iRen2)
+{
+    for(int i = 0; i < dMatriz[iRen1].size(); i++)
+    {
+        double dNumTemp = dMatriz[iRen1][i];
+        dMatriz[iRen1][i] = dMatriz[iRen2][i];
+        dMatriz[iRen2][i] = dNumTemp;
+    }
+}
+
 int main()
 {
     int iEcuaciones;
     int iVariables;
+		bool bNumeroCorrecto = false;
 
-    cout << "Cuantas ecuaciones tiene el sistema? ";
-    cin >> iEcuaciones;
-    cout << "Cuantas incognitas tiene el sistema? ";
-    cin >> iVariables;
+		// Pide el numero de ecuaciones y de incognitas, valida que sean iguales.
+		while(!bNumeroCorrecto)
+		{
+			cout << "Cuantas ecuaciones tiene el sistema? ";
+	    cin >> iEcuaciones;
+	    cout << "Cuantas incognitas tiene el sistema? ";
+	    cin >> iVariables;
+
+			if(iEcuaciones == iVariables)
+				bNumeroCorrecto = true;
+			else
+			{
+				cout << "Por favor ingrese otra vez, numero de ecuaciones tiene que " << endl;
+				cout << "ser igual al numero de incógnitas" << endl;
+			}
+		}
 
     // vector de vectores de enteros para guardar los datos
-    vector<vector<int>> iMatriz(iEcuaciones, vector<int>(iVariables + 1));
+    vector<vector<double>> dMatriz(iEcuaciones, vector<double>(iVariables + 1));
 
     for(int i = 0; i < iEcuaciones; i++)
     {
         for(int j = 0; j <= iVariables; j++)
         {
-            int iNumero;
+            double dNumero;
 
             if(j < iVariables)
                 cout << "Cual es el coeficiente de X" << j + 1 << "? ";
             else
                 cout << "Cual es el resultado de la ecuacion " << i + 1 << "? ";
 
-            cin >> iNumero;
+            cin >> dNumero;
 
-            iMatriz[i][j] = iNumero;
+            dMatriz[i][j] = dNumero;
         }
     }
+
+    intercambioRenglon(dMatriz, 0, 1);
 
     for(int i = 0; i < iEcuaciones; i++)
     {
         for(int j = 0; j <= iVariables; j++)
         {
-            cout << setw(3) << iMatriz[i][j] << ' ';
+            cout << setw(3) << dMatriz[i][j] << ' ';
         }
         cout << endl;
     }
